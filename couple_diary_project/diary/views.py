003f51cea50c2e3b1c466c862787ac35062162ff
minyou2675 <kymyy2675@gmail.com>
2023-06-19 14:08:24 +0900

    feat.달력이동 구현(달력 구현 사실상 끝)

--- couple_diary_project/diary/views.py
+++ couple_diary_project/diary/views.py
@@ -102,16 +102,20 @@ def showDailyDiary(request):
 
     mintDiary = Diary.objects.filter(author=mintUser,year=year,month=month,day=day)
     lemonDiary = Diary.objects.filter(author=lemonUser,year=year,month=month,day=day)
+      
     context = {'mintDiary' : mintDiary, 'lemonDiary' : lemonDiary}
     return render(request, 'diary/dailydiary.html',context)
 
 def moveCalendar(request,pk):
+    monthList = {1 : 'JAN', 2 : 'FEB', 3 : 'MAR', 4 : 'APR', 5 : 'MAY', 6 : 'JUN', 7 : 'JUL',
+                            8 : 'AUG', 9: 'SEP', 10 : 'OCT', 11: 'NOV', 12 : 'DEC'}
     date = str(pk)
     date = datetime.strptime(date,"%Y%m")
     year = date.year
     month = date.month   
     calendar = monthcalendar(year,month) 
     month_calendar = []
+    month_name = monthList[month]
     for week in calendar:   
         week_diary = []
         for day in week:
@@ -128,11 +132,11 @@ def moveCalendar(request,pk):
     print(month_calendar)
                          
         
-    return render(request,'diary/calandar.html',{'calendar':month_calendar,'year':year,'month':month})
+    return render(request,'diary/movecalendar.html',{'calendar':month_calendar,'year':year,'month':month,'month_name':month_name})
 
     print(date)
 
-def showCalandar(request):
+def showCalendar(request):
     year = datetime.today().year
     month = datetime.today().month
     calendar = monthcalendar(year,month)
@@ -153,7 +157,7 @@ def showCalandar(request):
     print(month_calendar)
                          
         
-    return render(request,'diary/calandar.html',{'calendar':month_calendar,'year':year,'month':month})
+    return render(request,'diary/calendar.html',{'calendar':month_calendar,'year':year,'month':month})
 
 def showQuestion(request):
     mintUser = User.objects.get(pk=1)
