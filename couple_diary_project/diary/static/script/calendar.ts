d5771b785ea5e3f8d9814344eb1e13430e7aaa73
minyou2675 <kymyy2675@gmail.com>
2023-08-07 22:08:50 -0700

    docker 추가

---
+++ couple_diary_project/diary/static/script/calendar.ts
@@ -0,0 +1,47 @@
+
+    // let date = new Date('{{year}}','{{month}}'-1);
+    // const first = new Date("2023-1-12");
+    // function onClickToList(){
+    //     window.location.href='/questionlist/1';
+    // }
+    // function onClickToDiary(){
+    //     window.location.href = '/dailydiary/';
+    // }
+    // function onClickToNext(){
+
+    //     oneMonthAfter = new Date(date.setMonth(date.getMonth()+1));
+    //     console.log(oneMonthAfter);
+    //     var next_year = oneMonthAfter.getFullYear();
+    //     var next_month = oneMonthAfter.getMonth()+1;
+    //     console.log(next_year,next_month);
+    //     window.location.href = `/calendar/${next_year}${next_month}`;
+
+    // }
+    // function onClickToPrev(){
+    //     oneMonthPrev = new Date(date.setMonth(date.getMonth()-1));
+    //     console.log(oneMonthPrev);
+    //     var prev_year = oneMonthPrev.getFullYear();
+    //     var prev_month = oneMonthPrev.getMonth()+1;
+    //     window.location.href = `/calendar/${prev_year}${prev_month}`;
+    //     console.log(prev_year,prev_month);
+    // }
+
+    // const renderDate = () => {
+    //     let nowDate = new Date();
+    //     const first = new Date("2023-1-12");
+    //     const viewYear = nowDate.getFullYear();
+    //     const viewMonth = nowDate.getMonth()+1;
+    //     const monthList = {1 : 'JAN', 2 : 'FEB', 3 : 'MAR', 4 : 'APR', 5 : 'MAY', 6 : 'JUN', 7 : 'JUL',
+    //                         8 : 'AUG', 9: 'SEP', 10 : 'OCT', 11: 'NOV', 12 : 'DEC'};
+    //     const todayMonth = monthList[viewMonth.toString()];
+
+    //     const diffDay= nowDate - first;
+    //     const dDay = Math.floor(diffDay  / (1000*60*60*24));
+        
+    //     document.querySelector('#dday').textContent = 'D-DAY'+' '+dDay;
+
+    // }
+
+    // renderDate();
+
+
